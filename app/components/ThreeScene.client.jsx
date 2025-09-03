1018027106a674aaf30fb7ba9dda3568b48a1410
evandayton762-tech <evandayton762@gmail.com>
2025-09-02 20:04:11 -0700

    Update ThreeScene.client.jsx

--- app/components/ThreeScene.client.jsx
+++ app/components/ThreeScene.client.jsx
@@ -1,3 +1,4 @@
+// app/components/ThreeScene.client.jsx
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/no-unused-vars */
 /* eslint-disable no-console */
@@ -6,28 +7,48 @@ import {Canvas} from '@react-three/fiber';
 import {OrbitControls, PerspectiveCamera} from '@react-three/drei';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 
+// Guard to skip this entire component during SSR
+function ClientOnlyRenderer({children}) {
+  if (import.meta.env.SSR) {
+    return null;
+  }
+  return children;
+}
+
 function GokuModel() {
   const [model, setModel] = useState(null);
+
   useEffect(() => {
-    new GLTFLoader().load('/models/goku.glb',
-      (gltf)=> setModel(gltf.scene),
+    new GLTFLoader().load(
+      '/models/goku.glb',            // 🔥 correct public path
+      (gltf) => setModel(gltf.scene),
       undefined,
-      (err)=> console.error(err));
+      (err) => console.error(err)
+    );
   }, []);
+
   if (!model) return null;
-  return <primitive object={model} scale={[3,3,3]} position={[0,-2,0]} />;
+  return <primitive object={model} scale={[3, 3, 3]} position={[0, -2, 0]} />;
 }
 
 export default function ThreeScene() {
   return (
-    <Canvas>
-      <PerspectiveCamera makeDefault position={[0,1.5,5]} />
-      <ambientLight intensity={0.3}/>
-      <directionalLight position={[10,10,5]} intensity={0.7}/>
-      <directionalLight position={[-10,-10,-5]} intensity={0.3}/>
-      <GokuModel/>
-      <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={0.5}
-        enablePan={false} minPolarAngle={Math.PI/3} maxPolarAngle={Math.PI/1.5}/>
-    </Canvas>
+    <ClientOnlyRenderer>
+      <Canvas>
+        <PerspectiveCamera makeDefault position={[0, 1.5, 5]} />
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[10, 10, 5]} intensity={0.7} />
+        <directionalLight position={[-10, -10, -5]} intensity={0.3} />
+        <GokuModel />
+        <OrbitControls
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={0.5}
+          enablePan={false}
+          minPolarAngle={Math.PI / 3}
+          maxPolarAngle={Math.PI / 1.5}
+        />
+      </Canvas>
+    </ClientOnlyRenderer>
   );
 }
